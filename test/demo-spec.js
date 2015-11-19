1707e20d43acb713498e5d1c33e35d9a17009fa7
Bryce Kolton <brycekolton@live.com>
2015-11-19 09:57:32 -0800

    laolsdoadlsao
    dhsiahdisahdisa

--- test/demo-spec.js
+++ test/demo-spec.js
@@ -1,2 +1,31 @@
 'use strict';
 
+describe('Twitter login form', function() {
+
+    it('should have a title of twitter login', function() {
+
+        browser.get('http://localhost:8000/');
+
+        expect(browser.getTitle()).toEqual('Firebase Demo');
+
+        
+    });
+
+    it('should let a user sign into an account', function() {
+
+        var emailInput = element(by.model('email'));
+        emailInput.sendKeys('test@example.com');
+        var button = element(by.buttonText('Sign-in'));
+        expect(button.isEnabled()).toEqual(false);
+        var passwordInput = element(by.model('password'))
+        passwordInput.sendKeys('password');
+        expect(button.isEnabled()).toEqual(true);
+
+
+        //button.click();
+
+        //var el = $('.')
+
+    });
+
+});
